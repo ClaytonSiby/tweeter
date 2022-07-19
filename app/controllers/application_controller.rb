d8f5c71ad4451e6a83c13d8a4d3e03dff381bfa0
Clayton Siby <claytonsiby@gmail.com>
2022-07-19 22:50:14 +0200

    Update the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "device"
+    else
+      "application"
+    end
+  end
 
   protected
 
