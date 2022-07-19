f6bddaa50bec97b798e1f6c16adf3d905cf14bad
Clayton Siby <claytonsiby@gmail.com>
2022-07-19 22:33:52 +0200

    Update the create action to add user on to the tweet hash on create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,13 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = Tweet.new(tweet_params.merge(user: current_user))
     respond_to do |format|
       if @tweet.save
         format.turbo_stream
@@ -24,6 +26,6 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweets).permit(:body)
+    params.require(:tweet).permit(:body)
   end
 end
