74a9c7b2ac816a915778dee6d89e0fe17a7aa72f
Clayton Siby <claytonsiby@gmail.com>
2022-07-19 21:43:38 +0200

    Add: Tweeter controller and configure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'tweet/index'
-  get 'tweet/create'
-  get 'tweet/destroy'
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
+  resources :tweets, except: [:edit, :update]
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'tweets#index'
 end
