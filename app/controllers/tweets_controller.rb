0a17e4652baa85f4f6c56433903e5658e1d14d63
Clayton Siby <claytonsiby@gmail.com>
2022-07-19 22:20:52 +0200

    Add: partial for the tweet form and update the index & create actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,29 @@
 class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweets).permit(:body)
   end
 end
